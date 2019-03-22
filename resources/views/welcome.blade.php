0d86ffd50f995f07398e2d85b5c9d1852aa08c3a
wangadd <1276321779@qq.com>
2019-03-22 06:37:58 +0000

    bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
                     @if($login==1)
                         <a href="http://hao.tactshan.com/menu">menu</a>
                     @else
-                        <a href="http://hao.tactshan.com/pclogin?url={{$url}}">login</a>
+                        <a href="http://hao.tactshan.com/login?url={{$url}}">login</a>
                         <a href="http://hao.tactshan.com/reg?url={{$url}}">Register</a>
                     @endif
                 </div>
